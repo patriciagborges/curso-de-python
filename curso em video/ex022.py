ae190018d0cfa95a9f274001e6a53af3240d95d4
patriciagborges <patriciagborges01@gmail.comclearuitquit>
2022-05-03 16:18:00 -0300

    exercicio 22

--- curso em video/ex022.py
+++ curso em video/ex022.py
@@ -4,4 +4,9 @@
 #quantas letras ao sem considerar o espaço
 #quantas letras tem o primeiro nome
 
-
+nome = str(input('Digite seu nome completo')).strip()
+print('Analisando seu nome...')
+print('Seu nome em maiúsculas é:{}'.format(nome.upper()))
+print('Seu nome em minusculas é:{}'.format(nome.lower()))
+print('Seu nome ao todo tem {} letras'.format(len(nome) - nome.count(' ')))
+print('Seu primeiro nome é {} e ele tem {} letras'.format(nome.split()[0], len(nome.split()[0])))
