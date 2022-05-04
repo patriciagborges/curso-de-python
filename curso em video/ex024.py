48150cd47c1eed2b205b24b13edd117554425157
patriciagborges <patriciagborges01@gmail.comclearuitquit>
2022-05-04 17:16:55 -0300

    exercicio 24

--- curso em video/ex024.py
+++ curso em video/ex024.py
@@ -1,2 +1,4 @@
 #Crie um programa que leia o nome de uma cidade e diga
 #se ela começa com a palavra "santo"
+cidade = input('Em qual cidade você nasceu?').strip()
+print(cidade[:5].upper() == 'SANTO')
