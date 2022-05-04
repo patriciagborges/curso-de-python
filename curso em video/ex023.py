cfaed950226c5cc4b91c56d53bbc69978343cdc6
patriciagborges <patriciagborges01@gmail.comclearuitquit>
2022-05-04 15:55:48 -0300

    exercicio 22

--- curso em video/ex023.py
+++ curso em video/ex023.py
@@ -1,7 +1,8 @@
-#Faça um programa que leia um numero de 0 a 9999 e mostre na tela
-#cada um dos digitos separados
+#Faça um programa que leia um numero de 0 a 9999 e mostre na tela cada um dos digitos separados
 #ex: digite 1867
 #unidade 4
-# dezena 3
+#dezena 3
 #centena 8
 #milhar 1
+
+n = int(input('digete um numero de até 4 dígitos'))
