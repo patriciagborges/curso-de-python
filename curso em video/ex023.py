24f825420ba456e43121c7ed46604d07290e52f6
patriciagborges <patriciagborges01@gmail.comclearuitquit>
2022-05-04 16:55:12 -0300

    exercicio 23

--- curso em video/ex023.py
+++ curso em video/ex023.py
@@ -6,3 +6,8 @@
 #milhar 1
 
 n = int(input('digete um numero de até 4 dígitos'))
+num = str(n)
+print('Unidade: {}'.format(num[3]))
+print('Dezena: {}'.format(num[2]))
+print('Centena: {}'.format(num[1]))
+print('Milhar: {}'.format(num[0]))
