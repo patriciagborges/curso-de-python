adaac2c6db0436790b431ed053bc55a0ac549a32
patriciagborges <patriciagborges01@gmail.comclearuitquit>
2022-05-04 18:10:06 -0300

    exercicio 26

--- curso em video/ex026.py
+++ curso em video/ex026.py
@@ -2,3 +2,8 @@
 #Quantas vezes aparece a letra "a"
 #em qual posição ela aparece na primeira vez
 #Em que posição ela aparece na ultima vez
+
+frase = str(input('Escreva uma frase')).upper().strip()
+print('A letra A aparece {} vezes'.format(frase.count('A')))
+print('A primeira letra A apareceu na posição {}'.format(frase.find('A')+1))
+print('A ultima letra A apareceu na posição {}'.format(frase.rfind('A')+1))
