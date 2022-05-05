9eb53af057cd51a3e3128783b19bdc54643e7ef5
patriciagborges <patriciagborges01@gmail.comclearuitquit>
2022-05-05 16:52:09 -0300

    Aula 10

---
+++ curso em video/aula10.py
@@ -0,0 +1,41 @@
+#condicionais
+#se carro.esquerda()
+#   bloco _v_
+#senao
+#   bloco_F_
+
+#Condição
+#if carro.esquerda():
+#   Bloco True
+#else:
+#   Bloco False
+
+#Ex:
+#tempo = int(input('quantos anos tem seu carro?'))
+#if tempo <=3:
+#    print('carro novo')
+#else:
+#    print('carro velho')
+#consição simplificada
+#tempo = int(input('quantos anos tem seu carro?'))
+#print('carro novo' if tempo <= 3 else 'carro velho')
+
+#ex1 condicional composta
+#nome = str(input('qual seu nome?'))
+#if nome == 'Gustavo':
+#    print('que belo nome!')
+#else:
+#    print('que nome feio')
+#print('Fim')
+
+#media
+#n1 = float(input('digite a primeira nota'))
+#n2 = float(input('Digite a segunda nota'))
+#media = (n1 + n2)/2
+#print('Sua média foi {}'.format(media))
+#if media >= 7.0:
+#    print('Passou de ano!')
+#else:
+#    print('Recuperação')
+
+
