682d09298875516744a4b0c7a9690d9a2abeab30
patriciagborges <patriciagborges01@gmail.comclearuitquit>
2022-05-05 17:59:30 -0300

    exercício 30

---
+++ curso em video/ex030.py
@@ -0,0 +1,8 @@
+#Crie um programa que leia um numero inteiro
+#e mostre ba tela se ele é par ou impar
+
+num = int(input('Digite um numero'))
+if num % 2 == 0:
+    print('Par')
+else:
+    print('impar')
