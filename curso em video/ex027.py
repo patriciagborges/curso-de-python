cb9ca7624a66e89eb902be6cff04e8b54d60d11c
patriciagborges <patriciagborges01@gmail.comclearuitquit>
2022-05-05 08:39:27 -0300

    exercicio 27

--- curso em video/ex027.py
+++ curso em video/ex027.py
@@ -3,3 +3,7 @@
 #ex: Ana Maria Braga
 #primeiro=Ana
 #ultimo = Braga
+n = str(input('Qual seu nome completo?')).strip()
+nome = n.split()
+print('Seu primeiro nome é {}'.format(nome[0]))
+print('Seu ultimo nome é {}'.format(nome[len(nome)-1]))
