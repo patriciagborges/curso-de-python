6ec643b1b76f5501cc8125144bf2536ac8ca6fd6
patriciagborges <patriciagborges01@gmail.comclearuitquit>
2022-05-23 17:44:38 -0300

    aula 11

---
+++ curso em video/aula11.py
@@ -0,0 +1,15 @@
+#cores no terminal
+#\033[0;33;44m
+#style
+#text
+#background
+
+#codigos de cores:
+#style: 0 1 4 7
+#text: 30 31 32 33 34 35 36 37
+#back: 40 41 42 43 44 45 46 47
+
+print('\033[0;30;41m Olá mundo!')
+
+
+
